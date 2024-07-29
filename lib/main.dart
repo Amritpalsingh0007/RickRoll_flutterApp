82b7e1edb12560b59f30c1593a100dd94c1a1a7a
Amritpalsingh0007 <amritpalsingh220923@gmail.com>
2024-07-29 09:01:53 +0530

    video layout column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -20,27 +21,23 @@ class Important extends StatefulWidget {
 class ImportantUi extends State<Important> {
   late VideoPlayerController _controller;
   late Future<void> _initializeVideoPlayerFuture;
+
   @override
   void initState() {
     super.initState();
-
-    // Create and store the VideoPlayerController. The VideoPlayerController
-    // offers several different constructors to play videos from assets, files,
-    // or the internet.
-    _controller = VideoPlayerController.asset("assets/video.mp4");
-
-    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller = VideoPlayerController.asset('assets/video.mp4');
+    _initializeVideoPlayerFuture = _controller.initialize().then((_) {
+      setState(() {});
+    });
   }
 
   @override
   void dispose() {
-    // Ensure disposing of the VideoPlayerController to free up resources.
     _controller.dispose();
-
     super.dispose();
   }
 
-  _load() {
+  void _load() {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (BuildContext context) {
       return Scaffold(
@@ -53,84 +50,84 @@ class ImportantUi extends State<Important> {
             foregroundColor: Colors.white,
             centerTitle: true,
           ),
-          body: FutureBuilder(
-            future: _initializeVideoPlayerFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                _controller.play();
-                return AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              // Wrap the play or pause in a call to `setState`. This ensures the
-              // correct icon is shown.
-              setState(() {
-                // If the video is playing, pause it.
-                if (_controller.value.isPlaying) {
-                  _controller.pause();
-                } else {
-                  // If the video is paused, play it.
+          body: Column(children: [
+            FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
                   _controller.play();
+                  return Center(
+                    child: AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      child: VideoPlayer(_controller),
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
                 }
-              });
-            },
-            // Display the correct icon depending on the state of the player.
-            child: Icon(
-              _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+              },
             ),
-          ));
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  if (_controller.value.isPlaying) {
+                    _controller.pause();
+                  } else {
+                    _controller.play();
+                  }
+                  debugPrint("isPlaying : ${_controller.value.isPlaying}");
+                });
+              },
+              child: Icon(
+                _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+              ),
+            ),
+          ]));
     }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Important App",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          "Important App",
+          style: TextStyle(fontWeight: FontWeight.bold),
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Center(
-                    child: Text(
-                  "This is the first app i build by my own(using internet) and it is doing something very important. click below to find out.",
-                  style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.italic),
-                )),
-                Divider(),
-                TextButton(
-                  style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white),
-                  onPressed: () {
-                    _load();
-                  },
-                  child: const Text(
-                    "Click here!",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ],
-            )));
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "This is the first app I built on my own (using the internet) and it is doing something very important. Click below to find out.",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            Divider(),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue, foregroundColor: Colors.white),
+              onPressed: _load,
+              child: const Text(
+                "Click here!",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
